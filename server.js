cbd1d477ea746ccb418c8281776e294bf23c77d1
BijanParsiyar <bparsiyar@gmail.com>
2018-05-21 03:19:12 -0500

    production server wildcard routes and serving up build folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -32,6 +33,19 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder, serve our production assets
+  app.use(express.static('client/build'));
+
+
+  // Express will serve up our index.html file
+  // if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+  });
+}
 
 const port = process.env.PORT || 5000;
 
